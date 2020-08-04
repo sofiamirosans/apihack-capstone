fa556607d133fcb86a88462c46b0d7d363180c4c
Sofi Mirosans <sofiamirosans@gmail.com>
2020-08-04 14:16:00 +0200

    server.js modifications

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const strava = require('strava-v3')
-strava.config({c2879ebb4098792b968d0a061c4403f663cf46a4})
-const payload = await strava.athlete.get('access_token')
-console.log(payload)
+const auth_link = "https://www.strava.com/oauth/token"
 
-const strava = require('strava-v3');
-strava.athlete.get({},function(err,payload,limits) {
-    if(!err) {
-        console.log(payload);
-    }
-    else {
-        console.log(err);
-    }
-});
+function getActivites(res){
 
-var strava = require('strava-v3')
-strava.config({
-  "access_token"  : "c2879ebb4098792b968d0a061c4403f663cf46a4",
-  "client_id"     : "51319",
-  "client_secret" : "bf2677a9816b7219d66140ae3bfb64de916d2e05",
-  "redirect_uri"  : "afb67a354f0275d0cf21146df85d73d3cf8fc4d3",
-});
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    fetch(activities_link)
+        .then((res) => console.log(res.json()))
+}
+
+function reAuthorize(){
+    fetch(auth_link,{
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+
+        },
+
+        body: JSON.stringify({
+
+            client_id: '51319',
+            client_secret: 'bf2677a9816b7219d66140ae3bfb64de916d2e05 ',
+            refresh_token: 'afb67a354f0275d0cf21146df85d73d3cf8fc4d3',
+            grant_type: 'refresh_token'
+        })
+    })
+    .then(res => getActivites(res))
+      
+}
 
